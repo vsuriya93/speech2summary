1c49d6441c48442851174d686a8c039f5244d94e
Suriyanarayanan Venkatesan <suriya@personal-mac-3.local>
2024-08-26 17:41:22 +0800

    refactor into utils

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-from faster_whisper import WhisperModel
+from utils import speech2text, rawtext2summary
 import json
 import os
+
 
 app = Flask(__name__)
 app.secret_key = 'sairam'  # Necessary for flashing messages
 
-model_size = "large-v3"
-model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-model_name = "t5-large"
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-summary_model = T5ForConditionalGeneration.from_pretrained(model_name)
 
 # Configure upload folder
 UPLOAD_FOLDER = 'uploads'
@@ -50,6 +44,7 @@ def upload_file():
             mimetype='application/json'
         )
         return response
+        # todo: design a landing page to render json response
         # return redirect(url_for('index'))
 
 
@@ -60,35 +55,10 @@ def process(file_path):
     process_metadata = {}
     process_metadata["file_path"] = file_path
 
-    # todo: add more checks here
-    segments, info = model.transcribe(file_path, beam_size=5)
-    print("Detected language '%s' with probability %f" %
-          (info.language, info.language_probability))
-
-    process_metadata["language"] = info.language
-    process_metadata["language_probability"] = info.language_probability
-
-    texts = []
-    for segment in segments:
-        print("[%.2fs -> %.2fs] %s" %
-              (segment.start, segment.end, segment.text))
-        texts.append(segment.text)
-
-    input_text_for_summarization = ''.join(' '.join(texts)).strip()
-    print("Original Text:")
-    print(input_text_for_summarization)
-
-    process_metadata["audio_file_summary"] = input_text_for_summarization
-
-    # Tokenize and summarize the input text using T5
-    inputs = tokenizer.encode("summarize: " + input_text_for_summarization,
-                              return_tensors="pt", max_length=1024, truncation=True)
-    summary_ids = summary_model.generate(inputs, max_length=1000, min_length=10,
-                                         length_penalty=2.0, num_beams=4, early_stopping=True)
-
-    # Decode and output the summary
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    print("\nSummary: ", summary)
-    process_metadata["t5_summary"] = summary
-
+    input_text_for_summarization = speech2text(file_path, process_metadata)
+    summary = rawtext2summary(input_text_for_summarization, process_metadata)
     return process_metadata
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
--- utils.py
+++ utils.py
@@ -1,1 +1,45 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 from faster_whisper import WhisperModel
+
+model_size = "large-v3"
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+model_name = "t5-large"
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+summary_model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+
+def speech2text(file_path, process_metadata) -> str:
+    # todo: add more checks here
+    segments, info = model.transcribe(file_path, beam_size=5)
+    print("Detected language '%s' with probability %f" %
+          (info.language, info.language_probability))
+
+    process_metadata["language"] = info.language
+    process_metadata["language_probability"] = info.language_probability
+
+    texts = []
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" %
+              (segment.start, segment.end, segment.text))
+        texts.append(segment.text)
+
+    input_text_for_summarization = ''.join(' '.join(texts)).strip()
+    print("Original Text:")
+    print(input_text_for_summarization)
+    process_metadata["audio_file_summary"] = input_text_for_summarization
+    return input_text_for_summarization
+
+
+def rawtext2summary(input_text, process_metadata) -> str:
+    # Tokenize and summarize the input text using T5
+    inputs = tokenizer.encode("summarize: " + input_text,
+                              return_tensors="pt", max_length=1024, truncation=True)
+    summary_ids = summary_model.generate(inputs, max_length=1000, min_length=10,
+                                         length_penalty=2.0, num_beams=4, early_stopping=True)
+
+    # Decode and output the summary
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    print("\nSummary: ", summary)
+    process_metadata["t5_summary"] = summary
+    return summary
